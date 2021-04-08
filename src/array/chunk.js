70fb00a620cfa85876ed96a49dcfea8a3bd17e8d
Evgeniy Talko <etalko@gmail.com>
2021-04-08 21:56:29 +0300

    refactor(chunk.test): write simple tests

--- src/array/chunk.js
+++ src/array/chunk.js
@@ -9,20 +9,16 @@ function chunk(array, size = 1) {
     const chunk = clonedArr.splice(0, size)
     chunkedArr.push(chunk)
 
-    console.log("CurrentCut: ", currentCut);
-    console.log("Current chunk: ", chunk);
-    console.log("ChunckedArr: ", chunkedArr);
+    if (process.env.DEBUG) {
+      console.log("CurrentCut: ", currentCut);
+      console.log("Current chunk: ", chunk);
+      console.log("ChunckedArr: ", chunkedArr);
+      console.log("===========================");
+    }
 
     if (currentCut >= cutsNumber) return [...chunkedArr, clonedArr];
-    // if (currentCut > array.length) return;
   }
 }
 
-// module.exports = chunk;
+module.exports = chunk;
 
-const array = [2, 3, 6, 10, 12, 40, 8, 5, 9, 33]
-const array2 = [2, 3, 6, 10, 12, 40, 8, 5, 9, 28, 10, 12, 40, 8, 5, 9, 33]
-const res = chunk(array, 3);
-const res2 = chunk(array2, 4);
-console.log("Result: ", res);
-console.log("Result: ", res2);
--- src/array/chunk.test.js
+++ src/array/chunk.test.js
@@ -1,4 +1,26 @@
 const chunk = require('./chunk')
 
 const array = [2, 3, 6, 10, 12, 40, 8, 5, 9, 33]
-console.log(chunk(array, 3));
+const arrayChuncked = [ [ 2, 3, 6 ], [ 10, 12, 40 ], [ 8, 5, 9 ], [ 33 ] ]
+const array2 = [2, 3, 6, 10, 12, 40, 8, 5, 9, 28, 10, 12, 40, 8, 5, 9, 33]
+const arrayChuncked2 = [
+  [ 2, 3, 6, 10 ],
+  [ 12, 40, 8, 5 ],
+  [ 9, 28, 10, 12 ],
+  [ 40, 8, 5, 9 ],
+  [ 33 ]
+]
+
+function chunkTest(array, chuncked, size){
+  const strChunked = JSON.stringify( chuncked )
+  const strChunkedFromFunc = JSON.stringify( chunk(array, size) );
+  test(`should return ${strChunked} `, () => {
+    expect(strChunkedFromFunc).toBe(strChunked)
+  })
+}
+
+describe('chunk():', () => {
+  chunkTest(array, arrayChuncked, arrayChuncked[0].length)
+  chunkTest(array2, arrayChuncked2, arrayChuncked2[0].length)
+}
+)
