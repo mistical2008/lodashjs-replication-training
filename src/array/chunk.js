a990ef26cf2b572287caf7b61702c7925a701672
Evgeniy Talko <etalko@gmail.com>
2021-04-09 16:38:53 +0300

    refactor(chunk): Write more performant algorithm

--- src/array/chunk.js
+++ src/array/chunk.js
@@ -1,25 +1,16 @@
 function chunk(array, size = 1) {
-  const clonedArr = array.slice();
-  const chunkedArr = [];
-  let currentCut = 0;
-  let cutsNumber = Math.trunc(array.length / size);
+  if (size === null || array.length === 0) return [];
 
-  while (true) {
-    currentCut++;
-    // TODO:  <08-04-21, Write more performant algorithm
-    // without splicing from the array start> //
-    const chunk = clonedArr.splice(0, size)
-    chunkedArr.push(chunk)
+  const chunckedLen = Math.ceil(array.length / size)
+  const chunked = Array(chunckedLen);
+  let chunckedIndex = 0;
+  let arrayIndex = 0;
 
-    if (process.env.DEBUG) {
-      console.log("CurrentCut: ", currentCut);
-      console.log("Current chunk: ", chunk);
-      console.log("ChunckedArr: ", chunkedArr);
-      console.log("===========================");
-    }
-
-    if (currentCut >= cutsNumber) return [...chunkedArr, clonedArr];
+  while (chunckedIndex < chunckedLen) {
+    chunked[chunckedIndex++] = array.slice(arrayIndex, arrayIndex += size)
   }
+
+  return chunked;
 }
 
 module.exports = chunk;
--- src/array/chunk.test.js
+++ src/array/chunk.test.js
@@ -1,26 +1,37 @@
 const chunk = require('./chunk')
 
 const array = [2, 3, 6, 10, 12, 40, 8, 5, 9, 33]
-const arrayChuncked = [ [ 2, 3, 6 ], [ 10, 12, 40 ], [ 8, 5, 9 ], [ 33 ] ]
+const arrayChuncked = [[2, 3, 6], [10, 12, 40], [8, 5, 9], [33]]
+
 const array2 = [2, 3, 6, 10, 12, 40, 8, 5, 9, 28, 10, 12, 40, 8, 5, 9, 33]
 const arrayChuncked2 = [
-  [ 2, 3, 6, 10 ],
-  [ 12, 40, 8, 5 ],
-  [ 9, 28, 10, 12 ],
-  [ 40, 8, 5, 9 ],
-  [ 33 ]
+  [2, 3, 6, 10],
+  [12, 40, 8, 5],
+  [9, 28, 10, 12],
+  [40, 8, 5, 9],
+  [33]
 ]
 
-function chunkTest(array, chuncked, size){
-  const strChunked = JSON.stringify( chuncked )
-  const strChunkedFromFunc = JSON.stringify( chunk(array, size) );
+const array3 = [2, 3, 6, 10, 12, 40, 8, 5, 9, 33]
+const arrayChuncked3 = [[2, 3], [6, 10], [12, 40], [8, 5], [9, 33]]
+
+const array4 = [];
+const arrayChuncked4 = [];
+
+function chunkTest(array, chuncked, size = null) {
+  const strChunked = JSON.stringify(chuncked)
+  const strChunkedFromFunc = JSON.stringify(chunk(array, size));
+  /* eslint-disable no-undef */
   test(`should return ${strChunked} `, () => {
     expect(strChunkedFromFunc).toBe(strChunked)
   })
 }
 
+// eslint-disable-next-line no-undef
 describe('chunk():', () => {
   chunkTest(array, arrayChuncked, arrayChuncked[0].length)
   chunkTest(array2, arrayChuncked2, arrayChuncked2[0].length)
+  chunkTest(array3, arrayChuncked3, arrayChuncked3[0].length)
+  chunkTest(array4, arrayChuncked4)
 }
 )
